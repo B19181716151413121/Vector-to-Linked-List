633c6a45c628b1e6a04371bf86fb0d1531a5fd8a
Brian <cobo.brian97@gmail.com>
2018-12-06 20:39:06 -0600

    Added Linked List Functions

--- Vector to Linked List/main.cpp
+++ Vector to Linked List/main.cpp
@@ -7,9 +7,74 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class NumberList
+{
+private:
+    struct ListNode
+    {
+        double value;
+        struct ListNode *next;
+    };
+    
+    ListNode *head;
+    
+public:
+    NumberList()
+    { head = nullptr; }
+    
+    ~NumberList();
+    
+    void appendNode(double num)
+    {
+        ListNode *newNode;
+        ListNode *nodePtr;
+        
+        newNode = new ListNode;
+        newNode->value = num;
+        newNode->next = nullptr;
+        
+        if (!head)
+            head = newNode;
+        else
+        {
+            nodePtr = head;
+            
+            while (nodePtr->next)
+                nodePtr = nodePtr->next;
+            
+            nodePtr->next = newNode;
+        }
+    }
+    
+    void displayList() const
+    {
+        ListNode *nodePtr;
+        
+        nodePtr = head;
+        
+        while (nodePtr)
+        {
+            cout << nodePtr->value << endl;
+            nodePtr = nodePtr->next;
+        }
+    }
+};
+
+int main()
+{
+    vector <vector <int> > vec = { {1}, {1,2}, {1,2,3} };
+    
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec[i].size(); j++)
+        {
+            cout << vec[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
